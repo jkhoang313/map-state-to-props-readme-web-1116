b5ea31d49bdaff23c67c47d30afb014fbd7ea8e6
jkhoang313 <jkhoang313@gmail.com>
2017-02-08 09:22:05 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,32 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom'
+// import configureStore from './store/configureStore';
+// import { createStore } from 'redux'
+// import shoppingListItemReducer from './reducers/shoppingListItemReducer'
+// import App from './App';
+// import './index.css';
+//
+// const store = createStore(shoppingListItemReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+//
+// ReactDOM.render(
+//     <App store={store}/>
+//   ,
+//   document.getElementById('root')
+// );
+
 import React from 'react';
 import ReactDOM from 'react-dom'
-import { createStore } from 'redux'
-import shoppingListItemReducer from './reducers/shoppingListItemReducer'
+import configureStore from './store/configureStore';
+import { Provider } from 'react-redux';
+import {getShoppingListItems} from './actions/shoppingListItemActions'
 import App from './App';
 import './index.css';
 
-const store = createStore(shoppingListItemReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const store = configureStore();
 
 ReactDOM.render(
-    <App store={store}/>
-  ,
+  <Provider store={store}>
+    <App store={store} />
+  </Provider>,
   document.getElementById('root')
 );
